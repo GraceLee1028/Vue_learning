da1567ef8840a785a1741962fcbeb804ef420fc7
lifeng <1924458755@qq.com>
2018-05-24 14:02:50 +0800

    vue学习

--- vueRouter/003/003-compiled.js
+++ vueRouter/003/003-compiled.js
@@ -6,38 +6,38 @@ var _Home2 = _interopRequireDefault(_Home);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//������������ԭ����
-// 2. ����·��
-// ÿ��·��Ӧ��ӳ��һ��������
-// ����"component" ������ͨ�� Vue.extend() ������������������
-// ���ߣ�ֻ��һ���������ö��󡾷�ʽһ����
-// ��������������Ƕ��·�ɡ�
-//��ʽ��
+//报错【不清楚原因】
+// 2. 定义路由
+// 每个路由应该映射一个组件。
+// 其中"component" 可以是通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象【方式一】。
+// 我们晚点再讨论嵌套路由。
+//方式二
 var MainExtend = Vue.extend(_Home2.default);
 //const routes = [
-//    {path:"/main",component:Home}//��ʽһ
-//    //{path:"/main",component:MainExtend}//��ʽ��
+//    {path:"/main",component:Home}//方式一
+//    //{path:"/main",component:MainExtend}//方式二
 //];
 
-// 3. ���� router ʵ����Ȼ���� `routes` ����
-// �㻹���Դ��������ò���, ��������ô�����Űɡ�
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
 /**
  * Created by SZ on 2018/5/8.
  */
-// 1. ���壨·�ɣ�������
-// ���Դ������ļ� import ����
+// 1. 定义（路由）组件。
+// 可以从其他文件 import 进来
 var router = new VueRouter({
-    routes: [{ path: "/main", component: _Home2.default //��ʽһ
-        //{path:"/main",component:MainExtend}//��ʽ��
+    routes: [{ path: "/main", component: _Home2.default //方式一
+        //{path:"/main",component:MainExtend}//方式二
     }]
-    // ����д���൱�� routes: routes
+    // （缩写）相当于 routes: routes
 });
 
-// 4. �����͹��ظ�ʵ����
-// �ǵ�Ҫͨ�� router ���ò���ע��·�ɣ�
-// �Ӷ�������Ӧ�ö���·�ɹ���
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
 var app = new Vue({
-    router: router //��дrouter
+    router: router //缩写router
 }).$mount('#app');
 
 //# sourceMappingURL=003-compiled.js.map
